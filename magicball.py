15fd002f8f59ce644429caa86dbd4e92455564da
Igor Loboda <i.loboda2024@gmail.com>
2025-12-15 01:13:50 +0200

    add numbers from 1 to 5

--- magicball.py
+++ magicball.py
@@ -5,3 +5,19 @@ question = "Yes or No"
 answer = ""
 random_number = random.randint(1,9)
 print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+else:
+  answer = "Error"
+
+print(name + " asks:" + "Will war end this year?")
+print("Magic 8-Ball's answer: " + answer)
